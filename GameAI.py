723ff966c4cb62645825b67a572e9c3a5036b70d
StevenHe918 <zhe5@uci.edu>
2018-11-10 12:40:58 -0800

    update

--- GameAI.py
+++ GameAI.py
@@ -1,4 +1,5 @@
 import random
+import 
 class AI:
     def __init__(self, mode:num, aphebet:list, word_bank:set): #easy:0, medium: 1, hard: 2
         self.aphebet = aphebet
@@ -29,7 +30,7 @@ class AI:
             if w_det < self.wmode:
                 return random.choice(self.aphebet) #xiaxuan
             else:
-                temp_bank = self.find_List(word)
+                
 
 
     
