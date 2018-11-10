19c0bfd382e6b26011301d1af7cf94f3476e8e94
StevenHe918 <zhe5@uci.edu>
2018-11-10 12:17:16 -0800

    update

--- GameAI.py
+++ GameAI.py
@@ -2,18 +2,27 @@ import random
 class AI:
     def __init__(self, mode:num): #easy:0, medium: 1, hard: 2
         if mode == 0:
-            self.mode = 10
+            self.cmode = 10
+            self.wmode = 50 #xiaxie
         else if mode == 1:
-            self.mode = 40
+            self.cmode = 40
+            self.wmode = 10 #xiaxie
         else:
-            self.mode = 80
+            self.cmode = 80
+            self.wmode = 0
 
         
-def AI(current_word):
-    challenge_det = random.randrange(0,100)
-    if challenge_det >=0 and challenge_det <10:
-        challenge_det = True
-    else:
-        challenge_det = False
+    def _challenge(self):
+        challenge_det = random.randrange(0,100)
+        if challenge_det >=0 and challenge_det < self.mode:
+            return True
+        else:
+            return False
+
+    def return_word(self, word): #1:"word_challenge" 2:"newWord"
+        if self._challenge():
+            return "_"
+        else:
+
 
     
