035fd154178a594bb64ec9e0c4b60f7ae37575c0
StevenHe918 <zhe5@uci.edu>
2018-11-10 12:44:03 -0800

    finish ai

--- GameAI.py
+++ GameAI.py
@@ -1,5 +1,5 @@
 import random
-import 
+from _Helper import *
 class AI:
     def __init__(self, mode:num, aphebet:list, word_bank:set): #easy:0, medium: 1, hard: 2
         self.aphebet = aphebet
@@ -30,6 +30,8 @@ class AI:
             if w_det < self.wmode:
                 return random.choice(self.aphebet) #xiaxuan
             else:
+                temp_set = find_prefix(word, self.word_bank)
+                return temp_set.pop()[len(word)]
                 
 
 
