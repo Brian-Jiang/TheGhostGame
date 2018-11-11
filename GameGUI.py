9ee89863acb867262a46eb7c957ad2e47a4e82a1
StevenHe918 <zhe5@uci.edu>
2018-11-10 16:02:27 -0800

    update GUI

--- GameGUI.py
+++ GameGUI.py
@@ -5,3 +5,16 @@ DEFAULT_FONT = ('Time', 24)
 class GameBoard:
     def __init__(self):
         self._dialog = tkinter.Tk()
+        self._title = tkinter.Label(
+            master = self._dialog, text = "Ghost Game",
+            font = DEFAULT_FONT
+        )
+        self._title.grid(
+            row = 0, column = 0, columnspan = 2, padx = 20, pady = 20,
+            sticky = tkinter.N
+        )
+
+
+
+if __name__ == '__main__':
+    GameBoard()
