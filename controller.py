2b2441cd942ed4624ea17778a8c337a9217904e5
yilei wu <yileiw5@uci.edu>
2018-11-10 21:26:38 -0800

    func

--- controller.py
+++ controller.py
@@ -1,13 +1,24 @@
 from GameLogic import GhostGame
 from GameAI import AI
 from _Helper import *
+import time
 from GameGUI import GameBoard
 
 alpha = 'qazwsxedcrfvtgbyhnujmikolp'
 
-if __name__ == "__main__":
-    word_bank = get_word_bank()
-    game = GhostGame(word_bank)
-    ai = AI(1, alpha, word_bank)
-    board = GameBoard()
-    board._dialog.mainloop()
+
+class controller:
+
+    def __int__(self):
+        self.word_bank = get_word_bank()
+        self.game = GhostGame(self.word_bank)
+        self.ai = AI(1, alpha, self.word_bank)
+
+
+    def func(self, word):#take a word
+        time.sleep(1)
+        if self.ai.challenge(word):
+            return '_'
+        else:
+            result = self.ai.return_word(word)
+            return result
