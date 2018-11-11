5e578be324b5d4470580610cead088372863d9b9
StevenHe918 <zhe5@uci.edu>
2018-11-10 20:05:03 -0800

    pip

--- GameGUI.py
+++ GameGUI.py
@@ -141,7 +141,7 @@ class GameBoard:
 
         canvas = tkinter.Canvas(
             master = self._dialog, width = 500, height = 500,
-            background = 'green',
+            background = 'grey',
         )
         canvas.grid(
             row = 1 , column = 0,
